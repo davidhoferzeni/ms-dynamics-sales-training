423ab4aa7d7a1c56ac0d9be806f44495b6d14c86
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-02-13 07:40:46 +0100

    Moved entities to dedicated directory

--- src/DynamicsToolLibrary/Model/Entities/AccountEntity.cs
+++ src/DynamicsToolLibrary/Model/Entities/AccountEntity.cs
@@ -5,11 +5,14 @@ public class AccountEntity : IDynamicsEntity
 {
     [DynamicsGuid]
     public Guid AccountId { get; set; }
+    
     [DynamicsColumn("name")]
     public string? AccountName { get; set; }
+    
     [DynamicsColumn("new_accountindex")]
     [DynamicsDataflow(DataflowType.Read)]
     public int? CurrentAccountIndex { get; set; }
+
     [DynamicsColumn("new_accountindex")]
     [DynamicsDataflow(DataflowType.Write)]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
